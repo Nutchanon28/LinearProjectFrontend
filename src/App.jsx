097c2ecc0731155a77571173bf1a7ca47484cd30
Nutchanon28 <65010290@kmitl.ac.th>
2023-10-13 14:19:57 +0700

    feat: created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
+        <div className="home">
+            <h1>Linear Project</h1>
+            <h2>Cant delete you so my mind, so I delete you from my photos.</h2>
+            <div className="appFormContainer">
+                <form className="appForm">
+                    <img src="preview photo" alt="preview photo" />
+                    <input required type="file" accept="image/*" />
+                    <label htmlFor="edgeDetectOptions">
+                        Edge Detection Options:
+                    </label>
+                    <select id="edgeDetectOptions" name="edgeDetect">
+                        <option value="canny">canny</option>
+                        <option value="laprician">laprician</option>
+                    </select>
+                    <button>Submit</button>
+                </form>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        </div>
     );
 }
 
