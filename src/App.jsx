b9f0bc41a21ea6a201cbdf4e12ed9a9563043fc3
Nutchanon28 <65010290@kmitl.ac.th>
2023-10-13 14:49:31 +0700

    feat: created submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+    const [avatarPath, setAvatarPath] = useState("");
+
+    const handleAvatarChange = (e) => {
+        console.log("img onChange triggered");
+        const file = e.target.files?.[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                setAvatarPath(reader.result);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("http://127.0.0.1:8000", {
+                test: "test",
+            });
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="home">
             <h1>Linear Project</h1>
             <h2>Cant delete you so my mind, so I delete you from my photos.</h2>
             <div className="appFormContainer">
-                <form className="appForm">
-                    <img src="preview photo" alt="preview photo" />
-                    <input required type="file" accept="image/*" />
+                <form className="appForm" onSubmit={handleSubmit}>
+                    <div className="appFormImgContainer">
+                        <img src={avatarPath} alt="preview photo" />
+                    </div>
+                    <input
+                        required
+                        type="file"
+                        accept="image/*"
+                        onChange={handleAvatarChange}
+                    />
                     <label htmlFor="edgeDetectOptions">
                         Edge Detection Options:
                     </label>
@@ -16,7 +54,7 @@ function App() {
                         <option value="canny">canny</option>
                         <option value="laprician">laprician</option>
                     </select>
-                    <button>Submit</button>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         </div>
